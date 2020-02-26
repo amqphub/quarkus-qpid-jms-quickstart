160937a93016ecca2419d89c9188c8029b7e6169
Robbie Gemmell <robbie@apache.org>
2020-02-26 14:23:35 +0000

    use quarkus-test-artemis for the test broker resource, simplifies the quickstart

--- src/test/java/org/acme/artemis/PriceTest.java
+++ src/test/java/org/acme/artemis/PriceTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.matchesPattern;
 
 import org.junit.jupiter.api.Test;
 
+import io.quarkus.artemis.test.ArtemisTestResource;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
